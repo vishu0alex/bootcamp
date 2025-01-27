166dd62337a93d474eb0edc1efce7979e2500676
vishu0alex <vikas8146033487@gmail.com>
2025-01-28 01:41:47 +0530

    Create hollowPyramidStar.c

---
+++ pattern/hollowPyramidStar.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(){
+  int n=5;
+  for(int i=1;i<=n;i++){
+
+    for(int j=1;j<=n-i;j++){
+        printf(" ");
+    }
+
+     for(int j=1;j<=2*i-1;j++){
+        if(j==1||j==2*i-1||i==n)
+        printf("*");
+        else printf(" ");
+    }
+
+    printf("\n");
+  }
+  
+  return 0;
+}
