cab249ac84aa996a6c83fd0b512fb28f1b5ce408
vishu0alex <vikas8146033487@gmail.com>
2025-01-30 19:50:52 +0530

    Create frequencyInSortedArray.c

---
+++ day4/frequencyInSortedArray.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int frequency(int arr[],int n){
+    int count=1;
+    for(int i=0;i<n;i++){
+    
+       if( arr[i]==arr[i+1])
+         count++;
+       if( arr[i]!=arr[i+1]){
+          printf("%d has frequency %d\n",arr[i],count);
+          count=1;
+       }
+    }
+}
+    
+int main(){
+    int n=8,arr[8]={1,2,2,3,3,4,4,4};
+    frequency(arr,n);
+    return 0;
+}
