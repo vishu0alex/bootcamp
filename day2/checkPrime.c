69c182689239f5ef50c4eca0ae130711799bc526
vishu0alex <vikas8146033487@gmail.com>
2025-02-02 01:40:29 +0530

    Update checkPrime.c

--- day2/checkPrime.c
+++ day2/checkPrime.c
@@ -1,19 +1,19 @@
 #include<stdio.h>
 int main(){
-    int n,count=0;
+    int n,flag=0;
     scanf("%d",&n);
 
    if(n==0||n==1)
    printf("not prime");
 
    for(int i=2;i<=n/2;i++){
-    if(n%i==0)
-        count++;
+    if(n%i==0){
+        flag=1;
+       break;
+    }
    }
-
-   if(count==0)
+   if(flag==0)
    printf("prime");
    else printf("not prime");
-
-    return 0;
+   return 0;
 }
