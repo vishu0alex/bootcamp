ae285549c282a97ad8b854ccf816e5da4c21b3bb
vishu0alex <vikas8146033487@gmail.com>
2025-02-06 11:20:24 +0530

    Create printBoundary.c

---
+++ day 9/printBoundary.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void printboundary(int arr[4][4],int n ,int m){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(i==0||i==n-1||j==0||j==m-1)
+            printf("%d ",arr[i][j]);
+            else printf("  ");
+        }
+        printf("\n");
+    }
+}
+int main(){
+    int n,m;
+    scanf("%d%d",&n,&m);
+    int arr[4][4];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    printboundary(arr,n,m);
+}
