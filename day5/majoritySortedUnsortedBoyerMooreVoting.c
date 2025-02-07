ff1b2fc8d222d35eefea8f5dae74c2e51ca17522
vishu0alex <vikas8146033487@gmail.com>
2025-02-08 00:29:24 +0530

    Update and rename majorityedit.c to majoritySortedUnsortedBoyerMooreVoting.c

--- day5/majoritySortedUnsortedBoyerMooreVoting.c
+++ day5/majoritySortedUnsortedBoyerMooreVoting.c
@@ -1,3 +1,4 @@
+//sorted
 #include<stdio.h>
 int majority(int arr[],int n){
     int count=1;
@@ -30,3 +31,37 @@ int main(){
     majority(arr,n);
     return 0;
 }
+
+//unsorted boyer moore voting algo
+
+#include<stdio.h>
+#include<limits.h>
+void majority(int arr[],int n){
+  int count=0,candidate;
+  for(int i=0;i<n;i++){
+    if(count==0){
+      candidate=arr[i];
+      count=1;
+    }
+    else if(arr[i]==candidate) count++;
+    else count--;
+  }
+  count=0;
+  for(int i=0;i<n;i++){
+    if(arr[i]==candidate)
+    count++;
+  }
+  if(count>n/2) printf("%d",candidate);
+  else printf("no majority");
+}
+int main(){
+  int n,d;
+  scanf("%d",&n);
+  int arr[n];
+  for(int i=0;i<n;i++){
+    scanf("%d",&arr[i]);
+  }
+  majority(arr,n);
+  return 0;
+}
+
