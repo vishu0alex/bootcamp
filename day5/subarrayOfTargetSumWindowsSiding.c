3610744ae263630d812ce1616ddf9808f5556ba9
vishu0alex <vikas8146033487@gmail.com>
2025-02-09 00:53:05 +0530

    Update subarrayOfTargetSumWindowsSiding.c

--- day5/subarrayOfTargetSumWindowsSiding.c
+++ day5/subarrayOfTargetSumWindowsSiding.c
@@ -26,3 +26,31 @@ int main(){
 
 //optimal
 
+#include<stdio.h>
+#include<limits.h>
+void subtargetsum(int arr[],int n,int sum){
+  int cursum=0,j=0;
+  for(int i=0;i<n;i++){
+    cursum+=arr[i];
+    while(cursum>sum&&j<=i){
+      cursum-=arr[j];
+      j++;
+    }
+    if(cursum==sum){
+    printf("found at index %d to %d",j,i);
+    return;
+    }
+  }
+  printf("not found");
+}
+int main(){
+  int n,sum;
+  scanf("%d%d",&n,&sum);
+  int arr[n];
+  for(int i=0;i<n;i++){
+    scanf("%d",&arr[i]);
+  }
+  subtargetsum(arr,n,sum);
+  return 0;
+}
+
